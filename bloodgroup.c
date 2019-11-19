dba32af535278ea15434304065a82cbfb5098581
BraveZzzz007 <gzgggameing@gmail.com>
2019-11-19 19:12:45 +0700

    modify bloodgroup.c by stdB

--- bloodgroup.c
+++ bloodgroup.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include <string.h>
 //Change here na ja
+//mai ru na
 void main()
 { char Name[40], G[5], answer[10];
   int i;
   do
   {
-    printf("What is your Name? ");
+    printf("What is your Name? "); 
     scanf("%s", Name);
     do
     {
